710f9c237c68622a2bb25e26a441a06c7993037d
Mamaeva Anastasiia <60839846+Brightest-Sunshine@users.noreply.github.com>
2021-09-27 08:26:10 +0300

    fix codestyle

--- task1/scr/task1_2.R
+++ task1/scr/task1_2.R
@@ -8,10 +8,8 @@ merge_dataframes <- function(dataframes){
 get_id <- function(dataframes){
   new_dataframe <- merge_dataframes(dataframes)
   dataframe_without_id <- new_dataframe[, -1]
-  means <- rowMeans(dataframe_without_id)
-  result <- data.frame(id=new_dataframe$id, mean_temp=means)
+  result <- data.frame(id=new_dataframe$id, mean_temp=rowMeans(dataframe_without_id))
   return(result)
-}
 
 main <- function(){
   load(path)
